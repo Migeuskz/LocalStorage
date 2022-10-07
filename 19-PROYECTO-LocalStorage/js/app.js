fd3fdba111f7f5b89db7c74d7afc9a2e7cbb4d83
Migeuskz <msanchezpapalotzi@gmail.com>
2022-10-06 22:07:18 -0500

    Validando Formulario

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -1,6 +1,6 @@
 //variables
 const formulario = document.querySelector('#formulario');
-const listaTweets = document.querySelector('#Lista-tweets');
+const listaTweets = document.querySelector('#lista-tweets');
 let tweets = [];
 
 //Event Listeners
@@ -15,6 +15,29 @@ function eventListeners(){
 //Funciones  
 function agregarTweet(e){
     e.preventDefault();
+    //Textarea donde el usuario escribe 
+    const tweet = document.querySelector('#tweet').value;
 
+    //Validación...
+    if(tweet === ''){
+        mostrarError('No puede ir vacio');
+        return;//evita que se ejecuten mas lineas de codigo
+    }
     console.log('Agregando Tweet');
+}
+
+//Mostrar mensaje de error
+function mostrarError(error){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    //insertando en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    //Elimina la alerta despues de 3 seg
+    setTimeout(() => {
+        mensajeError.remove();
+    },3000);
 }
